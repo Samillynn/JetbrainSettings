c57eb013d56a07b41eff1ed66a6edfa2ee893d23
samillynn <2837931428@qq.com>
2022-06-05 13:18:58 +0800

    CL-2022.1.2 <samill@samill Create vim_settings.xml, laf.xml, CP File.cpp, toolchains.xml, nodejs.xml, advancedSettings.xml, jdk.table.xml, C Source File.c, CP.cpp, find.xml, editor-font.xml, colors.scheme.xml, databaseDrivers.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/CP File.cpp
@@ -0,0 +1,25 @@
+#[[#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i ++)
+#define rep1(i, n) for(int i = 1; i <= (int)(n); i ++)
+#define rvs(i, n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define rvs1(i, n) for(int i = (int)(n); i >= 1; i--)
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+const char ret = '\n';
+
+const int N = 2e5;
+//#define int ll
+
+void solve() {
+
+}
+
+int32_t main() {
+        int T = 1;
+        cin >> T;
+        while (T--) solve();
+        return 0;
+}]]#
